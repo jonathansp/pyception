dee77994bd1660320f4bb976f7017afd81f7552f
Jonathan Prates <jonathan.prates@corp.globo.com>
2015-12-21 10:57:12 -0200

    feat: collections group

--- pyception/__init__.py
+++ pyception/__init__.py
@@ -2,11 +2,12 @@
 
 __author__ = 'Jonathan Simon Prates'
 __email__ = 'jonathan.simonprates@gmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __project__ = 'pyception'
 
 
 import pyception.application
+import pyception.collection
 import pyception.configuration
 import pyception.data
 import pyception.io
