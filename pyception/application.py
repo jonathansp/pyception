ffe5fef63320a4ce98c8dc1f4eb45533de835203
Jonathan Prates <jonathan.prates@corp.globo.com>
2015-12-21 10:58:22 -0200

    feat: added some ruby exceptions

--- pyception/application.py
+++ pyception/application.py
@@ -5,17 +5,18 @@ import sys
 from pyception.wrapper import PyceptionModule
 
 EXCEPTIONS = {
-    'ApplicationException':            (SystemError, 'The exception that is thrown when there is an generic application error.'),
     'AppDomainUnloadedException':      (SystemError, 'The exception that is thrown when an attempt is made to access an unloaded application.'),
+    'ApplicationException':            (SystemError, 'The exception that is thrown when there is an generic application error.'),
     'ArgumentOutOfRangeException':     (SystemError, 'The exception that is thrown when the value or argument is outside from allowed range.'),
     'CannotUnloadAppDomainException':  (SystemError, 'The exception that is thrown when an attempt to unload an application fails.'),
     'EntryPointNotFoundException':     (SystemError, 'The exception that is thrown when an attempt to load a class, plugin or module fails due to the absence of an method.'),
     'InvalidCastException':            (SystemError, 'The exception that is thrown for invalid casting or explicit conversion.'),
-    'InvalidOperationException':       (SystemError, 'The exception that is thrown when a method call is invalid for the object’s current state.'),
     'InvalidLibraryException':         (SystemError, 'The exception that is thrown when a library contains some invalid data.'),
+    'InvalidOperationException':       (SystemError, 'The exception that is thrown when a method call is invalid for the object’s current state.'),
     'MethodAccessException':           (SystemError, 'The exception that is thrown when there is an invalid attempt to access a private or protected method inside a class.'),
     'MissingMethodException':          (SystemError, 'The exception that is thrown when there is an attempt to access a method that does not exist.'),
-    'OperationAbortedException':       (SystemError, 'This exception is thrown when an operation is aborted.')
+    'OperationAbortedException':       (SystemError, 'This exception is thrown when an operation is aborted.'),
+    'SignalException':                 (SystemError, 'Raised when a signal is received.')
 }
 
 sys.modules[__name__] = PyceptionModule(__name__, EXCEPTIONS)
--- pyception/system.py
+++ pyception/system.py
@@ -8,7 +8,9 @@ EXCEPTIONS = {
     'AccessViolationException':      (SystemError, 'The exception that is thrown when data is read from or written to somewhere without explicit access.'),
     'ContextMarshalException':       (SystemError, 'The exception that is thrown when an attempt to marshal an object across a context boundary fails.'),
     'InsufficientMemoryException':   (SystemError, 'The exception that is thrown when there is not available memory to handle some data.'),
-    'PlatformNotSupportedException': (SystemError, 'The exception that is thrown when a feature does not run on a particular platform.')
+    'LocalJumpError':                (SystemError, 'Raised when it cannot yield as requested.'),
+    'PlatformNotSupportedException': (SystemError, 'The exception that is thrown when a feature does not run on a particular platform.'),
+    'SystemStackError':              (SystemError, 'Raised in case of a stack overflow.')
 }
 
 sys.modules[__name__] = PyceptionModule(__name__, EXCEPTIONS)
